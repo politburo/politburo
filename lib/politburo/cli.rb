44a939650262f3605851edc18578f153e2961f3e
Tal Rotbart <redbeard@gmail.com>
2012-08-23 15:24:07 +1000

    Create run directory and generate into it.

--- lib/politburo/cli.rb
+++ lib/politburo/cli.rb
@@ -1,5 +1,6 @@
 require 'trollop'
 require 'json'
+require 'pathname'
 
 module Politburo
   class CLI
@@ -17,7 +18,7 @@ module Politburo
         where [options] are:
         EOS
         opt :envfile, "Use a different envfile", :short => 'e', :default => 'Envfile'
-        opt :pretend, "Pretend to run, but don't actually execute any remote deps", :short => 'p'
+        opt :'babushka-sources-dir', "Use a different babushka sources dir", :short => 'b', :default => '~/.babushka/sources'
       end
 
       opts = Trollop::with_standard_exception_handling p do
@@ -37,6 +38,11 @@ module Politburo
 
     def run()
       #puts JSON.pretty_generate(root)
+      generate_to(target_generation_path.realpath)
+    end
+
+    def generate_to(target_dir)
+      File.open(File.join(target_dir, 'politburo_generated_deps.rb'), "w") { | f | f.write(root.to_babushka_deps) }
     end
 
     def root()
@@ -45,6 +51,21 @@ module Politburo
 
     def envfile_contents()
       @envfile_contents ||= File.open(options[:envfile], "r") { | f | f.read() }
+    end
+
+    def babushka_sources_path()
+      result_path = Pathname.new(File.expand_path(options[:'babushka-sources-dir']))
+      raise "Babushka sources directory: '#{result_path.to_s}' does not exist or isn't a directory" unless result_path.directory?
+      result_path
+    end
+
+    def target_generation_path()
+      @target_generation_path ||= begin 
+        target_generation_path = babushka_sources_path + "politburo-#{Time.now.to_i.to_s}"
+        target_generation_path.mkdir
+        raise "Could not create target generation path: '#{target_generation_path.realpath}'" unless target_generation_path.directory?
+        target_generation_path
+      end
     end
 
   end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,7 +3,7 @@ require 'stringio'
 
 describe Politburo::CLI do 
 
-  context "#run" do
+  context "normal running" do
 
     let (:cli) { Politburo::CLI.new(options, targets) }
     let (:targets) { ['fake-target'] }
@@ -29,13 +29,101 @@ ENVFILE_CONTENTS
 
     before(:each) do
       cli.stub(:envfile_contents).and_return(envfile_contents)
-      cli.run
     end
 
-    context "envfile" do
+    context "#root" do
       it "should parse the envfile contents into the root context" do
         cli.root.should_not be_nil
         cli.root.children.size.should == 2
+      end
+
+    end
+
+    context "#babushka_sources_path" do
+
+      let (:fake_babushka_sources_pathname) { double("fake babushka sources pathname") }
+
+      before :each do
+        cli.options.should_receive(:[]).with(:'babushka-sources-dir').and_return(:fake_babushka_sources_dir)
+        File.should_receive(:expand_path).with(:fake_babushka_sources_dir).and_return(:fake_expanded_babushka_sources_dir)
+        Pathname.should_receive(:new).with(:fake_expanded_babushka_sources_dir).and_return(fake_babushka_sources_pathname)
+      end
+
+      it "should use the babushka-sources-dir option to construct the babushka dir" do
+        fake_babushka_sources_pathname.should_receive(:directory?).and_return(true)
+
+        cli.babushka_sources_path.should == fake_babushka_sources_pathname
+      end
+
+      it "should raise an error if the end result isn't a valid directory" do
+        fake_babushka_sources_pathname.should_receive(:directory?).and_return(false)
+
+        lambda { cli.babushka_sources_path}.should raise_error
+      end
+
+    end
+
+    context "#target_generation_path" do
+
+      let (:fake_babushka_sources_pathname) { double("fake babushka sources pathname") }
+      let (:fake_target_path) { double("fake target path") }
+      let (:fake_time) { double('fake time') }
+      let (:fake_time_as_i) { double('fake time as integer') }
+
+      before :each do
+        cli.should_receive(:babushka_sources_path).and_return(fake_babushka_sources_pathname)
+        
+        Time.should_receive(:now).and_return(fake_time) 
+        fake_time.should_receive(:to_i).and_return(fake_time_as_i)
+        fake_time_as_i.should_receive(:to_s).and_return('generated-timestamp')
+
+        fake_babushka_sources_pathname.should_receive(:+).with('politburo-generated-timestamp').and_return(fake_target_path)
+
+        fake_target_path.should_receive(:mkdir)
+        fake_target_path.stub(:directory?).and_return(true)
+      end
+
+      it "should use the babushka_sources_dir and timestamp to generate a target dir" do
+        cli.target_generation_path.should == fake_target_path
+      end
+
+      it "should raise an error if the directory wasn't created" do
+        fake_target_path.should_receive(:directory?).and_return(false)
+
+        lambda { cli.target_generation_path }.should raise_error
+      end
+
+      it "should be memoised so it returns the same target path the 2nd time around" do
+        cli.target_generation_path.should == cli.target_generation_path
+      end
+
+    end
+
+    context "#generate_to" do
+
+      let(:file_double) { double("fake file") }
+      let(:target_dir) { double("fake dir s") }
+
+      it "should write the babushka deps into a file in the specified source directory" do
+        cli.root.should_receive(:to_babushka_deps).and_return(:fake_deps_s)
+        File.should_receive(:join).with(target_dir, 'politburo_generated_deps.rb').and_return(:fake_combined_path)
+        File.should_receive(:open).with(:fake_combined_path, "w").and_yield(file_double)
+        file_double.should_receive(:write).with(:fake_deps_s)
+
+        cli.generate_to(target_dir)
+      end
+
+    end
+
+    context "#run" do
+      let (:fake_target_path) { double("fake target path") }
+
+      it "should generate the babushka deps file to the target directory" do
+        cli.should_receive(:target_generation_path).and_return(fake_target_path)
+        fake_target_path.should_receive(:realpath).and_return(:fake_target_realpath)
+        cli.should_receive(:generate_to).with(:fake_target_realpath)
+
+        cli.run
       end
 
     end
@@ -61,6 +149,27 @@ ENVFILE_CONTENTS
 
         it "should set the envfile" do
           cli.options[:envfile].should == 'ProvidedEnvfile'
+        end
+
+      end
+
+    end
+
+    context "--babushka-sources-dir" do
+
+      context "when not provided" do
+        let(:args) { %w(fake-target) }
+
+        it "should default to ~/.babushka/sources" do
+          cli.options[:'babushka-sources-dir'].should == '~/.babushka/sources'
+        end
+      end
+
+      context "when provided" do
+        let(:args) { %w(--babushka-sources-dir=Some-dir fake-target) }
+
+        it "should set the source dir" do
+          cli.options[:'babushka-sources-dir'].should == 'Some-dir'
         end
 
       end
