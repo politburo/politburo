b1e294817d67da3cce770502e07d464d2d79b497
Tal Rotbart <redbeard@gmail.com>
2012-10-22 16:25:28 +1100

    Added tests around state_task

---
+++ spec/resource/state_task_spec.rb
@@ -0,0 +1,20 @@
+describe Politburo::Resource::StateTask do
+
+  let(:resource) { Politburo::Resource::Base.new(name: "Resource") }
+
+  let(:state) { Politburo::Resource::State.new(resource: resource, name: "state") }
+
+  let(:state_task) { Politburo::Resource::StateTask.new(state) }
+
+  it "should initialize correctly" do
+    state_task.resource_state.should be state
+  end
+
+  context "#resource" do
+
+    it "should return the associated resource" do
+      state_task.resource.should be resource
+    end
+
+  end
+end
