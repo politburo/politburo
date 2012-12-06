cc6444b91019328bd9613469cb1f00d60a380053
Tal Rotbart <redbeard@gmail.com>
2012-12-06 13:58:45 +1100

    Changed availability zone to a symbol

--- spec/integration/Amazon.envfile.rb
+++ spec/integration/Amazon.envfile.rb
@@ -1,6 +1,6 @@
 environment(name: 'Amazon', description: "Amazon integration test environment", flavor: :amazon_web_services) do
 
-  { "US West" => 'us-west-1', "US East" => 'us-east-1', "APAC Sydney" => 'ap-southeast-2' }.each_pair do 
+  { "US West" => :'us-west-1', "US East" => :'us-east-1', "APAC Sydney" => :'ap-southeast-2' }.each_pair do 
     | name, availability_zone |
 
     facet(name: name, availability_zone: availability_zone) do
