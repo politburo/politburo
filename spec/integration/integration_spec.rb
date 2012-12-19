94a744715a2637be32602cd56d282480a481b7dd
Tal Rotbart <redbeard@gmail.com>
2012-12-19 15:51:28 +1100

    Better end to end integration test.

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -4,48 +4,45 @@ describe "End to end test" do
 
   let (:cli) { Politburo::CLI.create(arguments) }
 
-	let(:arguments) { "-e #{environment_definition_file} #ready".split(/\s/) }
-
-	describe "with simple environment" do
-
-		let(:environment_definition_file) do
-			File.join(File.dirname(__FILE__), "Simple.envfile.rb")
-		end
-
-		it "should run the envfile correctly", :skip => true do
-			cli.run.should be_true
-		end
-	end
-
-	describe "with complex environment" do
-
-		let(:environment_definition_file) do
-			File.join(File.dirname(__FILE__), "Complex.envfile.rb")
-		end
-
-		it "should run the envfile correctly", :skip => true do
-			cli.run.should be_true
-		end
-	end
+  let(:command_line) { "-e #{environment_definition_file} ##{state_to_achieve}" }
+	let(:arguments) { command_line.split(/\s/) }
 
 	describe "with Amazon environment" do
-
 		let(:environment_definition_file) do
 			File.join(File.dirname(__FILE__), "Amazon.envfile.rb")
 		end
 
 		let(:test_host) do
-			cli.root.context.lookup(name: "Primary host in zone", availability_zone: :'ap-southeast-2')
+			cli.root.context.lookup(name: "Primary host in zone", availability_zone: 'ap-southeast-2')
 		end
 
-		it "should have an AWS cloud provider" do
-			test_host.cloud_provider.should be_a Politburo::Resource::Cloud::AWSProvider
+		context "#ready" do
+			let(:state_to_achieve) { "ready" }
+
+			it "should have an AWS cloud provider" do
+				test_host.cloud_provider.should be_a Politburo::Resource::Cloud::AWSProvider
+			end
+
+			it "should run the envfile correctly" do
+				test_host.should_not be_nil
+				cli.run.should be_true
+
+				test_host.cloud_server.should_not be_nil
+				test_host.cloud_server.state.should == "running"
+			end
 		end
 
-		it "should run the envfile correctly" do
-			test_host.should_not be_nil
-			cli.run.should be_true
+		context "#stopped" do
+			let(:state_to_achieve) { "stopped" }
+
+			it "should run the envfile correctly" do
+				test_host.should_not be_nil
+				cli.run.should be_true
+
+				test_host.cloud_server.should_not be_nil
+				test_host.cloud_server.state.should == "stopped"
+			end
 		end
+
 	end
-
 end
